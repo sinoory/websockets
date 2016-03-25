147976776486088e12259d5c64522b400bb735b1
test <sin@test.com>
2016-03-25 09:29:46 +0800

    test tst/tstserver.cpp run ok

--- tstcpp/tst/tstserver.cpp
+++ tstcpp/tst/tstserver.cpp
@@ -2,6 +2,23 @@
 
 
 class MultyClient: public Client{
+    public:
+    virtual void onMessage(unsigned char*msg,int len){
+        unsigned char m[30]={0};
+        memcpy(m,msg,len);
+        printf("---onMessage len=%d m=%s\n",len,m);
+        char* repmsg=(char*)"srv_msg_from_onMessage";
+        sendMessage(repmsg,strlen(repmsg));
+
+	    char buf[256];
+        char ip[50];
+        getClientIp(ip,sizeof(ip));
+        lwsl_notice("----onMessage from %s\n", ip);
+
+    };
+    virtual void onClose(){printf("---client %d onClose\n",getCurrentFd());};
+
+
 };
 
 int main(){
--- tstcpp/websocketserver.h
+++ tstcpp/websocketserver.h
@@ -17,20 +17,8 @@ struct per_session_data__echo {
 
 class Client{
     public:
-    virtual void onMessage(unsigned char*msg,int len){
-        unsigned char m[30]={0};
-        memcpy(m,msg,len);
-        printf("---onMessage len=%d m=%s\n",len,m);
-        char* repmsg=(char*)"srv_msg_from_onMessage";
-        sendMessage(repmsg,strlen(repmsg));
-
-	    char buf[256];
-        char ip[50];
-        getClientIp(ip,sizeof(ip));
-        lwsl_notice("----onMessage from %s\n", ip);
-
-    };
-    virtual void onClose(){printf("---client %d onClose\n",getCurrentFd());};
+    virtual void onMessage(unsigned char*msg,int len){};
+    virtual void onClose(){};
 
     void sendMessage(char* msg,int len){
 		memcpy(&mCurrentSessionData->buf[LWS_PRE],msg, len);
